cbb5ae9d9ff58c737b75a7b69fa2cf2dab49fff0
Dante De Ruwe <dantederuwe@gmail.com>
2021-08-19 10:30:00 +0200

    Refactor appshell entrypoints

--- src/appshell/BoardgameStore.Client/Program.cs
+++ src/appshell/BoardgameStore.Client/Program.cs
@@ -1,15 +1,9 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Json;
-using System.Reflection;
-using System.Runtime.Loader;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using MicrofrontendFramework.Blazor;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BoardgameStore.Client
 {
@@ -27,33 +21,8 @@ namespace BoardgameStore.Client
             var client = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
             builder.Services.AddScoped<HttpClient>(_ => client);
 
-            var assemblies = await LoadAssembliesAsync(client, builder.HostEnvironment.IsDevelopment());
-            builder.Services.AddMicrofrontends(assemblies); //the magic
-        }
-
-        private static async Task<IEnumerable<Assembly>> LoadAssembliesAsync(HttpClient client, bool isDevelopment = true)
-        {
-            var filePaths = await client.GetFromJsonAsync<string[]>("/api/assemblies");
-            var dllPaths = filePaths?.Where(r => r.EndsWith(".dll"));
-
-            var clientAssembly = Assembly.GetAssembly(typeof(App));
-            var assemblies = new List<Assembly> { clientAssembly };
-
-            if (dllPaths is null) return assemblies;
-
-            foreach (var dllPath in dllPaths)
-            {
-                var pdbPath = Regex.Replace(dllPath, @"\.dll$", ".pdb");
-                var pdbShouldBeLoaded = isDevelopment && filePaths.Contains(pdbPath);
-
-                await using var dllStream = await client.GetStreamAsync(dllPath);
-                await using var pdbStream = pdbShouldBeLoaded ? await client.GetStreamAsync(pdbPath) : null;
-
-                var assembly = AssemblyLoadContext.Default.LoadFromStream(dllStream, pdbStream);
-                assemblies.Add(assembly);
-            }
-            
-            return assemblies;
+            var assemblies = await AssemblyLoader.LoadAssembliesAsync(client, builder.HostEnvironment.IsDevelopment());
+            builder.Services.AddMicrofrontends(assemblies);
         }
     }
 }
--- src/appshell/BoardgameStore.Server/Startup.cs
+++ src/appshell/BoardgameStore.Server/Startup.cs
@@ -1,17 +1,11 @@
-using BoardgameStore.Client;
+using System.IO;
+using MicrofrontendFramework.Blazor;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
-using System.Text.RegularExpressions;
-using MicrofrontendFramework.Blazor;
 
 namespace BoardgameStore.Server
 {
@@ -31,7 +25,7 @@ namespace BoardgameStore.Server
 
             services.AddSelfReferentialHttpClient();
 
-            var assemblies = LoadAssemblies();
+            var assemblies = ServerAssemblyLoader.LoadAssemblies();
             services.AddMicrofrontends(assemblies);
         }
 
@@ -69,31 +63,6 @@ namespace BoardgameStore.Server
                 endpoints.MapControllers();
                 endpoints.MapFallbackToPage("/_Host");
             });
-        }
-
-        private static IEnumerable<Assembly> LoadAssemblies()
-        {
-            var dllPaths = Directory.GetFiles(@"CDN")
-                .Where(f => f.EndsWith("dll"))
-                .ToList();
-
-            var clientAssembly = Assembly.GetAssembly(typeof(App));
-            var assemblies = new List<Assembly> { clientAssembly };
-
-            if (dllPaths.Count < 1) return assemblies;
-
-            foreach (var dllPath in dllPaths)
-            {
-                var pdbPath = Regex.Replace(dllPath, @"\.dll$", ".pdb");
-
-                using var pdbStream = File.Exists(pdbPath) ? File.Open(pdbPath, FileMode.Open) : null;
-                using var dllStream = File.Open(dllPath, FileMode.Open);
-
-                var assembly = AssemblyLoadContext.Default.LoadFromStream(dllStream, pdbStream);
-                assemblies.Add(assembly);
-            }
-            
-            return assemblies;
         }
     }
 }
