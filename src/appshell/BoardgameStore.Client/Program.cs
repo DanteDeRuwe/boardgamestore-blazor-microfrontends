deaf83535aefe422270280c5193f5db0aeaedb80
Dante De Ruwe <dante.deruwe@mediahuis.be>
2024-05-29 10:55:02 +0200

    Small tweaks

--- src/appshell/BoardgameStore.Client/Program.cs
+++ src/appshell/BoardgameStore.Client/Program.cs
@@ -9,9 +9,8 @@ CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("nl-BE");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("nl-BE");
 
 var client = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-builder.Services.AddScoped<HttpClient>(_ => client);
-
 var assemblies = await ClientAssemblyLoader.LoadAssembliesAsync(client, builder.HostEnvironment.IsDevelopment());
+
 builder.Services.AddMicrofrontends(assemblies);
 
 var app = builder.Build();
